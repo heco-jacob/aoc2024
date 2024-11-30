463021eb74962e743851fdfacfed87b795ba13e6
Jacob Wang <jacob.wang@hafslund.no>
2024-11-30 23:56:54 +0100

    Yadda yadda

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -1,6 +1,19 @@
+use std::fs;
+use std::env;
+use crate::utils::say_hello;
+
+
 mod utils;
 
+const INPUT_DIR: &str = "../inputs"; // Adjust the relative path as needed
+
+
 fn main() {
-    println!("Hello, world!");
     utils::say_hello();
+    let file_path = format!("{}/day1_input.txt", INPUT_DIR);
+    let input = fs::read_to_string(file_path).expect("Could not read file");
+
+    for line in input.lines() {
+        println!("{}", line);
+    }
 }
--- day-01/src/bin/part2.rs
+++ day-01/src/bin/part2.rs
@@ -1,1 +1,6 @@
+mod utils;
 
+fn main() {
+    println!("Hello, world!");
+    utils::say_hello();
+}
