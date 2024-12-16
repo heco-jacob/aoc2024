c9c5aaca0ab7732e58952c7da569b25bb1e61c26
Jacob Wang <jacob.wang@hafslund.no>
2024-12-16 16:36:21 +0100

    Fixed erronous scoring for id>9.

--- day-09/src/bin/part2.rs
+++ day-09/src/bin/part2.rs
@@ -5,8 +5,7 @@ fn sequence_to_string(seq: &Vec<(Option<i32>, i32, bool)>) -> String {
 
     for (id, size, is_file) in seq.iter() {
         if *is_file {
-            // File blocks are represented by their ID
-            let char_rep = id.unwrap_or(0).to_string(); // Use 0 as default if no ID
+            let char_rep = id.unwrap_or(0).to_string();
             result.push_str(&char_rep.repeat(*size as usize));
         } else {
             result.push_str(&".".repeat(*size as usize));
@@ -15,15 +14,10 @@ fn sequence_to_string(seq: &Vec<(Option<i32>, i32, bool)>) -> String {
     result
 }
 
-fn main() {
-    let mut score: i64 = 0;
-    let input: String = read_input_to_string(9);
-    let charvec: Vec<char> = input.chars().collect();
-
+fn diskmap_to_files(char_vec: Vec<char>) -> Vec<(Option<i32>, i32, bool)> {
     let mut seq: Vec<(Option<i32>, i32, bool)> = vec![]; // (id: i32, size: i32, is_file: bool)
-
     let mut id = 0;
-    for (idx, c) in charvec.iter().enumerate() {
+    for (idx, c) in char_vec.iter().enumerate() {
         if let Some(digit) = c.to_digit(10) {
             if idx % 2 == 0 {
                 seq.push((Some(id), digit as i32, true));
@@ -33,6 +27,35 @@ fn main() {
             }
         }
     }
+    seq
+}
+
+fn to_char_vec(input: &str) -> Vec<char> {
+    input.chars().collect()
+}
+
+fn scoring(seq: &Vec<(Option<i32>, i32, bool)>) -> i64 {
+    let mut score: i64 = 0;
+    let mut index = 0;
+    for (id, size, _) in seq.iter() {
+        if let Some(id_int) = *id {
+            for _ in 0..*size {
+                score += id_int as i64 * index as i64;
+                index += 1;
+            }
+        } else {
+            index += size;
+        }
+    }
+    score
+}
+
+fn main() {
+    let mut score: i64 = 0;
+    let input: String = read_input_to_string(9);
+    let char_vec: Vec<char> = to_char_vec(&input);
+
+    let mut seq = diskmap_to_files(char_vec);
 
     let mut file_ids: Vec<_> = seq
         .iter()
@@ -41,7 +64,7 @@ fn main() {
         .collect();
 
     for id in file_ids.iter().rev() {
-        println!("String: {}", sequence_to_string(&seq));
+        // println!("String: {}", sequence_to_string(&seq));
 
         if let Some(file_index) = seq.iter().position(|(seq_id, _, _)| seq_id == &Some(*id)) {
             let file = seq[file_index];
@@ -50,7 +73,6 @@ fn main() {
                 .position(|(seq_id, size, is_file)| size >= &file.1 && !is_file)
             {
                 let space = seq[space_index];
-                // println!("File: {:?} at file index {:?}, space: {:?} at space index {:?}", file, file_index, space, space_index);
                 if space_index > file_index {
                     continue;
                 }
@@ -58,7 +80,6 @@ fn main() {
                 seq[file_index] = (None, file.1, false);
                 let diff = (file.1 - space.1).abs();
                 if diff > 0 {
-                    // println!("Diff is {:?}, adding new space", diff);
                     let new_space: (Option<i32>, i32, bool) = (None, diff, false);
                     seq.insert(space_index + 1, new_space);
                 }
@@ -66,15 +87,70 @@ fn main() {
             }
         }
     }
+    score += scoring(&seq);
+    println!("Part 2: {}", score);
+}
 
-    // scoring
-    let s = sequence_to_string(&seq);
-    for (idx, c) in s.chars().enumerate() {
-        if let Some(digit) = c.to_digit(10) {
-            let add = digit as i64 * idx as i64;
-            score += add;
-        }
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_to_char_vec() {
+        let string = "1234";
+        let char_vec: Vec<char> = to_char_vec(string);
+        assert_eq!(char_vec, vec!['1', '2', '3', '4']);
     }
 
-    println!("Part 2: {}", score);
+    #[test]
+    fn test_diskmap_to_files() {
+        let char_vec: Vec<char> = to_char_vec("12345");
+        let files = diskmap_to_files(char_vec);
+        assert_eq!(
+            files,
+            vec![
+                (Some(0), 1, true),
+                (None, 2, false),
+                (Some(1), 3, true),
+                (None, 4, false),
+                (Some(2), 5, true)
+            ]
+        );
+
+        let char_vec: Vec<char> = to_char_vec("101");
+        let files = diskmap_to_files(char_vec);
+        assert_eq!(
+            files,
+            vec![(Some(0), 1, true), (None, 0, false), (Some(1), 1, true)]
+        );
+
+        let char_vec: Vec<char> = to_char_vec("010");
+        let files = diskmap_to_files(char_vec);
+        assert_eq!(
+            files,
+            vec![(Some(0), 0, true), (None, 1, false), (Some(1), 0, true)]
+        );
+    }
+
+    #[test]
+    fn test_seq_to_string() {
+        let char_vec: Vec<char> = to_char_vec("12345");
+        let files = diskmap_to_files(char_vec);
+        let string = sequence_to_string(&files);
+        assert_eq!(string, "0..111....22222");
+
+        let char_vec: Vec<char> = to_char_vec("010204");
+        let files = diskmap_to_files(char_vec);
+        let string = sequence_to_string(&files);
+        assert_eq!(string, ".......");
+
+        let char_vec: Vec<char> = to_char_vec("00011");
+        let files = diskmap_to_files(char_vec);
+        let string = sequence_to_string(&files);
+        assert_eq!(string, ".2");
+
+        let char_vec: Vec<char> = to_char_vec("2333133121414131402");
+        let files = diskmap_to_files(char_vec);
+        let string = sequence_to_string(&files);
+        assert_eq!(string, "00...111...2...333.44.5555.6666.777.888899");
+    }
 }
